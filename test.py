279253da2a90fec11506eceb2793410c79f1d315
Denis <denis_kolobov.internet.ru>
2024-03-25 02:51:00 +1000

    Correct style for tests

--- test.py
+++ test.py
@@ -28,7 +28,8 @@ class TestSuite(unittest.TestCase):
 
     def set_valid_auth(self, request):
         if request.get("login") == api.ADMIN_LOGIN:
-            request["token"] = hashlib.sha512((datetime.datetime.now().strftime("%Y%m%d%H") + api.ADMIN_SALT).encode(encoding='utf-8')).hexdigest()
+            request["token"] = hashlib.sha512((datetime.datetime.now().strftime("%Y%m%d%H") + api.ADMIN_SALT)
+                                              .encode(encoding='utf-8')).hexdigest()
         else:
             msg = request.get("account", "") + request.get("login", "") + api.SALT
             request["token"] = hashlib.sha512(msg.encode(encoding='utf-8')).hexdigest()
